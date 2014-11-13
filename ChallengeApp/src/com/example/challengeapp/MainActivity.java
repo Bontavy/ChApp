8bc9f523405031c3665ae30aa7ebd29ccfcf33ee
michael-kaldawi <mxk123330@utdallas.edu>
2014-11-13 17:00:20 -0600

    michael changed Challenge.java

--- ChallengeApp/src/com/example/challengeapp/MainActivity.java
+++ ChallengeApp/src/com/example/challengeapp/MainActivity.java
@@ -10,15 +10,28 @@
 
 package com.example.challengeapp;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
+	EditText Username;
+	EditText Password;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,8 +44,87 @@ public class MainActivity extends Activity {
      * @exception Exception throws an exception
      */
     public void goToCreateAChallenge(View view) throws Exception {
+    	Username = (EditText) findViewById(R.id.username);
+    	Password = (EditText) findViewById(R.id.password);
+    	Boolean LoginSuccess = false;
+    	File file = new File("Account.txt");
+    	
+    	//if(!file.exists())
+    	/*{
+    		try {
+        		FileOutputStream fOut = openFileOutput("Account.txt", MODE_WORLD_READABLE);
+        		OutputStreamWriter osw = new OutputStreamWriter(fOut);
+        		os..write("Read");
+        		osw.flush();
+        		osw.close();
+        	}catch(FileNotFoundException e)
+        	{
+        		 e.printStackTrace();
+        	}
+    		//Toast.makeText(getApplicationContext(), "creta", Toast.LENGTH_SHORT).show();
+    	}*/
+    	
+    	{
+    		String test = "";
+    		InputStream iS = openFileInput("Account.txt");
+    		FileInputStream fIn = openFileInput("Account.txt");
+    		InputStreamReader isr = new InputStreamReader(iS);
+    		BufferedReader read = new BufferedReader(isr);
+    		String receiveString = "";
+            StringBuilder stringBuilder = new StringBuilder();
+             
+            while ( (receiveString = read.readLine()) != null ) {
+                stringBuilder.append(receiveString);
+            }
+             
+            iS.close();
+            test = stringBuilder.toString();
+            Toast.makeText(getApplicationContext(), test, Toast.LENGTH_SHORT).show();
+        
+    	}
+    	
+    	/*try {
+    		FileOutputStream fOut = openFileOutput("Account.txt", MODE_WORLD_READABLE);
+    		OutputStreamWriter osw = new OutputStreamWriter(fOut);
+    		fOut.
+    		osw.write("");
+    		osw.flush();
+    		osw.close();
+    	}catch(FileNotFoundException e)
+    	{
+    		 e.printStackTrace();
+    	}*/
+    	if(LoginSuccess)
+    	{
     	Intent intent = new Intent(MainActivity.this, CreateAChallengeActivity.class);
     	startActivity(intent);
+    	}
+    }
+    
+    
+    public void createAccount(View view) throws Exception {
+    	Username = (EditText) findViewById(R.id.username);
+    	Password = (EditText) findViewById(R.id.password);
+    	Boolean LoginSuccess = false;
+    	
+    	//if(!file.exists())
+    	{
+    		try {
+        		FileOutputStream fOut = openFileOutput("Account.txt", MODE_APPEND);
+        		OutputStreamWriter osw = new OutputStreamWriter(fOut);
+        		osw.append("< ");
+        		osw.append(Username.getText().toString());
+        		osw.append(Password.getText().toString());
+        		osw.append(" >");
+        		osw.flush();
+        		osw.close();
+        	}catch(FileNotFoundException e)
+        	{
+        		 e.printStackTrace();
+        	}
+    		//Toast.makeText(getApplicationContext(), "creta", Toast.LENGTH_SHORT).show();
+    	}
+    	Toast.makeText(getApplicationContext(), "Account Created", Toast.LENGTH_SHORT).show();
     }
     
     
